2452e10de8d862b673d6557fbe956e8b1c5050e1
Colin Armstrong <colinwarmstrong@gmail.com>
2018-10-14 22:01:55 -0600

    Creates server.js file.

--- db/seeds/dev/01_meals.js
+++ db/seeds/dev/01_meals.js
@@ -2,7 +2,7 @@ exports.seed = function(knex, Promise) {
   // We must return a Promise from within our seed function
   // Without this initial `return` statement, the seed execution
   // will end before the asynchronous tasks have completed
-  return knex('meals').del() // delete all footnotes first
+  return knex('meals').del() // delete all meals first
 
     // Now that we have a clean slate, we can re-insert our meal data
     .then(() => {
