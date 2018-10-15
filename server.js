578e492f39d083e4192bdac7f26235f09ebf1555
Colin Armstrong <colinwarmstrong@gmail.com>
2018-10-14 23:37:18 -0600

    Sets up test file and successfully tests the GET /api/v1/foods/:id endpoint.

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('./knexfile')[environment]
+const database = require('knex')(configuration)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('port', process.env.PORT || 3000)
+app.locals.title = 'Quantified Self'
+
+app.get('/api/v1/foods/:id', (request, response) => {
+  database('foods').where('id', request.params.id).select()
+    .then(food => {
+      if (food.length) {
+        response.status(200).json(food.pop())
+      } else {
+        response.status(404).json({
+          error: `Could not find food with id ${request.params.id}`
+        })
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+})
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`)
+})
+
+module.exports = app
---
+++ test/routes.spec.js
@@ -0,0 +1,47 @@
+const chai = require('chai')
+const should = chai.should()
+const chaiHttp = require('chai-http')
+const server = require('../server')
+
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('../knexfile')[environment]
+const database = require('knex')(configuration)
+
+chai.use(chaiHttp)
+
+describe('API Routes', () => {
+  before((done) => {
+    database.migrate.latest()
+      .then(() => done())
+      .catch(error => {
+        throw error
+      })
+  })
+
+  beforeEach((done) => {
+    database.seed.run()
+      .then(() => done())
+      .catch(error => {
+        throw error
+      })
+  })
+
+  describe('GET /api/v1/foods/:id', () => {
+    it('should return the paper with the specified id', done => {
+      chai.request(server)
+        .get('/api/v1/foods/1')
+        .end((err, response) => {
+          response.should.have.status(200)
+          response.should.be.json
+          response.body.should.be.an('object')
+          response.body.should.have.property('id')
+          response.body.id.should.equal(1)
+          response.body.should.have.property('name')
+          response.body.name.should.equal('eggs')
+          response.body.should.have.property('calories')
+          response.body.calories.should.equal(100)
+          done()
+        })
+    })
+  })
+})
